e61fe779556188492537b30a64c589922560fe8c
Ives <jianxing.imo99g@gmail.com>
2022-07-25 23:22:41 +0800

    fix: 加入新增訂單方法

--- javasoapclient/src/main/java/com/bharah/ws/soap/CustomerOrderWsClient.java
+++ javasoapclient/src/main/java/com/bharah/ws/soap/CustomerOrderWsClient.java
@@ -5,10 +5,13 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
+import com.bharath.ws.trainings.CreateOrdersRequest;
+import com.bharath.ws.trainings.CreateOrdersResponse;
 import com.bharath.ws.trainings.CustomerOrdersPortType;
 import com.bharath.ws.trainings.GetOrdersRequest;
 import com.bharath.ws.trainings.GetOrdersResponse;
 import com.bharath.ws.trainings.Order;
+import com.bharath.ws.trainings.Product;
 
 public class CustomerOrderWsClient {
 
@@ -16,13 +19,48 @@ public class CustomerOrderWsClient {
 		// step 1 設定 WS位置
 		CustomerOrdersWsImplService service = new CustomerOrdersWsImplService(new URL("http://localhost:8080/wsdlfirstws/customerordersservice?wsdl"));
 		CustomerOrdersPortType customerOrdersWsImplPort = service.getCustomerOrdersWsImplPort();
+		List<Order> orders = getOrderList(customerOrdersWsImplPort);
+		System.out.println("Number of orders for the customer are:"+ orders.size());
+		
+		boolean result = createOrder(customerOrdersWsImplPort);
+		System.out.println("The new order has been sent:"+ result);
+	}
+	
+	/**
+	 * 取得訂單列表
+	 * @param portType
+	 * @return
+	 */
+	private static List<Order> getOrderList(CustomerOrdersPortType portType){
 		// step 2 設定 request
 		GetOrdersRequest request = new GetOrdersRequest();
 		request.setCustomerId(BigInteger.valueOf(1));
 		// step 3 從response取得 orders
-		GetOrdersResponse response = customerOrdersWsImplPort.getOrders(request);
-		List<Order> orders = response.getOrder();
-		System.out.println("Number of orders for the customer are:"+ orders.size());
+		GetOrdersResponse response = portType.getOrders(request);
+		return response.getOrder();		
+	}
+	
+	/**
+	 * 新增訂單
+	 * @param portType
+	 * @return
+	 */
+	private static boolean createOrder(CustomerOrdersPortType portType) {
+		// step 2 設定 request
+		CreateOrdersRequest request = new CreateOrdersRequest();
+		request.setCustomerId(BigInteger.valueOf(1));
+		Order order = new Order();
+		order.setId(BigInteger.valueOf(2));
+		List<Product> products = order.getProduct();
+		Product product = new Product();
+		product.setId("2");
+		product.setDescription("MAC BOOK PRO");
+		product.setQuantity(BigInteger.valueOf(5));
+		products.add(product);
+		request.setOrder(order);
+		// step 3 從response新增 order
+		CreateOrdersResponse response = portType.createOrders(request);
+		return response.isResult();
 	}
 
 }
