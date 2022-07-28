0d89e9b00ad1e8f11c102069273a5e09902b27aa
Ives <jianxing.imo99g@gmail.com>
2022-07-29 00:05:14 +0800

    fix: 設定比對callback密碼

--- javafirstws/src/main/java/com/bharath/ws/soap/config/UTPasswordCallBack.java
+++ javafirstws/src/main/java/com/bharath/ws/soap/config/UTPasswordCallBack.java
@@ -1,17 +1,38 @@
 package com.bharath.ws.soap.config;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
 
-public class UTPasswordCallBack implements CallbackHandler {
+import org.apache.wss4j.common.ext.WSPasswordCallback;
 
+public class UTPasswordCallBack implements CallbackHandler {
+	
+	private Map<String,String> passwords = new HashMap<>();
+	
+	public UTPasswordCallBack() {
+		// TODO 正常情況下是使用DB進行相關資料的取得
+		passwords.put("bharath", "bharath");
+		passwords.put("cxf", "cxf");
+	}
+	
+	/**
+	 * 比對password callback
+	 */
 	@Override
 	public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
-		// TODO Auto-generated method stub
-
+		for(Callback callback : callbacks) {
+			WSPasswordCallback passwordCallback = (WSPasswordCallback) callback;
+			String password = passwords.get(passwordCallback.getIdentifier());
+			if(password!=null) {
+				passwordCallback.setPassword(password);
+				return;
+			}
+		}
 	}
 
 }
