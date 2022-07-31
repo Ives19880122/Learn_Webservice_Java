d0f590e49ec6a1a8c6484b5ccb5b5358fd77ce90
Ives <jianxing.imo99g@gmail.com>
2022-07-31 17:44:07 +0800

    feat: 建立新的REST WS
    1. pom.xml專案建立
    2. 設置model與service架構

---
+++ restws/src/main/java/com/bharah/restws/PatientService.java
@@ -0,0 +1,8 @@
+package com.bharah.restws;
+
+import javax.ws.rs.Path;
+
+@Path("/patientService") // 標註Service EndPoint
+public interface PatientService {
+	
+}
---
+++ restws/src/main/java/com/bharah/restws/PatientServiceImpl.java
@@ -0,0 +1,8 @@
+package com.bharah.restws;
+
+import org.springframework.stereotype.Service;
+
+@Service // Spring使用
+public class PatientServiceImpl implements PatientService {
+	
+}
