a79a1bd25b042c6bd7d8f71461ad203d981b843b
Ives <jianxing.imo99g@gmail.com>
2022-07-31 17:49:15 +0800

    fix: 設置Service init方法

--- restws/src/main/java/com/bharah/restws/PatientServiceImpl.java
+++ restws/src/main/java/com/bharah/restws/PatientServiceImpl.java
@@ -1,8 +1,27 @@
 package com.bharah.restws;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
+
+import com.bharah.restws.model.Patient;
 
 @Service // Spring使用
 public class PatientServiceImpl implements PatientService {
 	
+	Map<Long,Patient> patients = new HashMap<>();
+	long currentId = 123;
+	
+	public PatientServiceImpl() {
+		init();
+	}
+	
+	// mock data from database
+	void init() {
+		Patient patient = new Patient();
+		patient.setId(currentId);
+		patient.setName("JavaRS");
+		patients.put(patient.getId(), patient);
+	}
 }
